2a4b2e88072b0adddef5975920d151fa1852eba5
Aviv Kandabi <kandabiaviv@gmail.com>
2024-01-13 16:26:53 +0200

    rename and deleted files

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { sum } from "../src/sum";
+import { sum } from "../lib/sum";
 
 describe("add", () => {
   it("should sum of 2 and 3 equals to 5", () => {
